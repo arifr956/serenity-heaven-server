18aa57d271a5f3200a1cea004154e490dbbaad77
Arifur Rahman <arifr956@gmail.com>
2023-11-26 19:47:46 +0600

    user stored in database

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+//const cookieParser = require('cookie-parser');
+//const jwt = require('jsonwebtoken');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors(
+    //{
+//     origin: [
+//       'http://localhost:5173',
+//       //'https://study-mate-client.web.app',
+//       //`https://study-mate-client.firebaseapp.com`,
+//   ],
+//   //credentials: true
+ // }
+  ));
+  app.use(express.json());
+
+  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moucvko.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const userCollection = client.db("serenityHeaven").collection("user");
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
